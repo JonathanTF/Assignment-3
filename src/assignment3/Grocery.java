ec6739ee6ac075aaec1d6f2f746187eaf6f49c5a
royceli804 <royceli804@hotmail.com>
2016-02-20 17:23:23 -0600

    toUpperCase
    Changed toLowerCase to toUpperCase

--- src/assignment3/Grocery.java
+++ src/assignment3/Grocery.java
@@ -6,12 +6,12 @@ public class Grocery extends Item {
 	//override calculatePrice() if necessary; Implement print methods as necessary	
 	// Only re-implement stuff you cannot get from the superclass (Item)
 	public Grocery(	String new_name, float new_price, int new_quantity, float new_weight, String op1){
-		op1  = op1.toLowerCase();
+		op1  = op1.toUpperCase();
 		name = new_name;
 		price = new_price;
 		quantity = new_quantity;
 		weight = new_weight;
-		if (op1 == "p"){
+		if (op1 == "P"){
 			perishable = true;
 		}
 		else{
