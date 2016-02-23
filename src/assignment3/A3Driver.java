e53cc6b3f0cd8c8c9cd578e84c87dd6fa8037524
Jonathan Friesen <jonny28f@gmail.com>
2016-02-22 18:14:54 -0600

    Large change to main and insert
    Changed the way main looked, although it should have the same kind of
    functionality as it did before. I thought it would be kind of neat to
    use an iterator to go through the transactions list. Used switch
    statement to do the "if-else-if-else.." for each type of transaction.
    Put in skeletons for the search, delete, update, and print methods and
    added the shopping cart input argument for each of them. Wrote insert(),
    but haven't thoroughly tested it yet.

--- src/assignment3/A3Driver.java
+++ src/assignment3/A3Driver.java
@@ -16,24 +16,33 @@ public class A3Driver
 	  {
 		/*Open file; file name specified in args (command line)*/
 		String file_name = args[0];
-		int index = 1; //keep track of what transaction we are on
 		ArrayList<String> input_transactions = getFromFile(file_name);// returns an array of the transactions in an array of Strings
+		Iterator<String> t = input_transactions.iterator();// use an iterator to run through the list of transactions
+		ArrayList<Item> shoppingCart = new ArrayList<Item>();// the shopping cart
 		String delims = "[ ]+";	
-		while (index-1 <= input_transactions.size()){
-			String str = input_transactions.get(index-1);
-			String[] tokens = str.split(delims);
-			tokens[3] = tokens[3];
+		while (t.hasNext()){// while there is a next element
+			String str = t.next();// this will bring up the next transaction
+			String[] tokens = str.split(delims);// each transaction is split into 'tokens' array
+			tokens[3] = tokens[3];  // what does this do?***
 			boolean check = errorCheck(tokens);
 			if(check){
-				if (tokens[0] == "insert"){
-					insert(tokens);
+				switch (tokens[0]) {// case argument for the 5 types of transactions
+					case "insert": insert(tokens, shoppingCart);
+								   break;
+					case "search": search(tokens, shoppingCart);
+								   break;
+					case "delete": delete(tokens, shoppingCart);
+								   break;
+					case "update": update(tokens, shoppingCart);
+								   break;
+					case "print": print(tokens, shoppingCart);
+								  break;
 				}
 			}
-			//Parse input, take appropriate actions.
 		}
 		
 		  
-		//Stub for arraylist.
+		/*//Stub for arraylist.
 		ArrayList<Item> shoppingCart = new ArrayList<Item>(); 
 		
 		// General code example for how to iterate an array list. You will have to modify this heavily, to suit your needs.
@@ -47,13 +56,15 @@ public class A3Driver
 			//based on the inherited class type, as to which method is to be invoked. Eg: If it is an instance
 			// of Grocery, it will invoke the calculatePrice () method defined in Grocery.
 		}	
-	
+		*/
 
 	  
 	}
 	  
 	  
-	  
+
+
+
 	/******************************************************************************
 	* Method Name: getFromFile                                                    *
 	* Purpose: Reads through the input file and converts each line into an array  *							  
@@ -151,12 +162,70 @@ public class A3Driver
 	 }
 	 
 	 /******************************************************************************
-		* Method Name: insert                                                    *
-		* Purpose: parses through tokens array for the insert transaction
-		******************************************************************************/
+	 * 	Method Name: insert														   *
+	 *	 Purpose: parses through tokens array for the insert transaction           *
+	 * @param input_transactions                                                   *
+	 ******************************************************************************/
 	 
-	 public static void insert(String[] tokens){
-		 return; 
+	 public static void insert(String[] tokens, ArrayList<Item> shopping_cart){
+		 Float priceFloat = Float.parseFloat(tokens[3]);
+		 int quantityInt = Integer.parseInt(tokens[4]);
+		 int weightInt = Integer.parseInt(tokens[5]);
+		 if(tokens[1] == "groceries"){
+			Item temp = new Grocery(tokens[2], priceFloat, quantityInt, weightInt, tokens[6]);
+			shopping_cart.add(temp);
+			return;
+		 }else if(tokens[1] == "electronics"){
+			Item temp = new Electronics(tokens[2], priceFloat, quantityInt, weightInt, tokens[6], tokens[7]);
+			shopping_cart.add(temp);
+			return;
+		 }else if(tokens[1] == " clothing"){
+			Item temp = new Clothing(tokens[2], priceFloat, quantityInt, weightInt);
+			shopping_cart.add(temp);
+			return;
+		 } 
 	 }
 	 
+	 
+	 /******************************************************************************
+	 * 	Method Name: print														   *
+	 *	 Purpose: print the contents of the shopping cart by name		           *
+	 * @param shopping_cart                                                        *
+	 ******************************************************************************/
+	private static void print(String[] tokens, ArrayList<Item> shopping_cart) {
+		return;
+	}
+	
+	
+	 /******************************************************************************
+	 * 	Method Name: update														   *
+	 *	 Purpose: updates the quantity field for the first occurrence              *
+	 *   of a matching name                                                        *
+	 * @param shopping_cart                                                        *
+	 ******************************************************************************/
+	private static void update(String[] tokens, ArrayList<Item> shopping_cart) {
+		return;
+	}
+	
+	
+	 /******************************************************************************
+	 * 	Method Name: delete                                                        *
+	 *	 Purpose: would delete all entries the same input name, and output to the  *
+	 *   screen how many were deleted                                              *
+	 ******************************************************************************/
+	private static void delete(String[] tokens, ArrayList<Item> shopping_cart) {
+		return;
+	}
+	
+	
+	 /******************************************************************************
+	 * 	Method Name: search														   *
+	 *	 Purpose: searches for all objects with name field as <name> and then      *
+     *   outputs the number of objects found to the screen                         *
+	 * @param shopping_cart                                                        *
+	 ******************************************************************************/
+	private static void search(String[] tokens, ArrayList<Item> shopping_cart) {
+		return;
+	}
+ 
 }
